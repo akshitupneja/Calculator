2075b81c801dfa91d61f98b04f75f1a9b9b7806b
akshitupneja <31714187+akshitupneja@users.noreply.github.com>
2017-09-25 18:09:27 -0400

    Adding Calculator Functionality using MVC. Basic functionality is working.

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -8,13 +8,89 @@
 
 import Foundation
 
+
+
 struct CalculatorBrain {
     
     private var accumulator: Double?
     
-    func performOperation(_ symbol : String ) {
-        
+    private enum Operation {
+        case constant (Double)
+        case unaryOperation ((Double) -> (Double))
+        case binaryOperation ((Double,Double) -> Double)
+        case equals
+        case clear
     }
+    
+    
+    private var operations: Dictionary<String,Operation> = [
+        
+        "√": Operation.unaryOperation(sqrt),
+        "+/-": Operation.unaryOperation({ -$0 }),
+        "+": Operation.binaryOperation({ $0 + $1 }),
+        "-": Operation.binaryOperation({ $0 - $1 }),
+        "÷": Operation.binaryOperation({ $0 / $1 }),
+        "x": Operation.binaryOperation({ $0 * $1 }),
+        "=": Operation.equals,
+        "V": Operation.clear
+        
+    ]
+    
+ 
+    private var pbo : PendingBinaryOperation?
+    
+    
+    private struct PendingBinaryOperation {
+        let function : (Double,Double) -> Double
+        let firstOperand : Double
+        
+        func perform(with secondOperand: Double) -> Double {
+            return function(firstOperand,secondOperand)
+        }
+    }
+    
+    
+    
+    
+    mutating func performOperation(_ symbol : String ) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
+            
+            case .binaryOperation(let function) :
+                if accumulator != nil {
+                    pbo = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    accumulator = nil
+                }
+        
+            case .equals :
+                performPendingBinaryOperation()
+            
+            case .clear :
+                clear()
+                
+            default:
+                break
+            }// end of switch
+        }//end of if
+        
+    }// end of function
+    
+    
+    
+    
+    private mutating func performPendingBinaryOperation(){
+        if pbo != nil && accumulator != nil {
+            accumulator = pbo!.perform(with: accumulator!)
+            pbo = nil
+        }// End of if
+    } //end of function
+    
+    
+    
     
     mutating func setOperand(_ operand: Double){
         accumulator = operand
@@ -26,4 +102,9 @@ struct CalculatorBrain {
         }
     }
 
+    mutating private func clear() {
+        accumulator = nil
+        pbo = nil
+
+    }
 }
